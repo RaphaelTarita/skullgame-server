9b2d026dfcbe323f48076e25bc667af6fcabe303
Raphael Tarita <raphael.tarita@gmail.com>
2023-09-08 17:44:11 +0200

    version upgrades: skullgame-common: 1.0.0 -> 1.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val kotlinVersion = "1.9.10"
 val ktorVersion = "2.3.4"
 val logbackVersion = "1.4.11"
 val kotlinxDatetimeVersion = "0.4.1"
-val skullgameCommonVersion = "1.0.0"
+val skullgameCommonVersion = "1.0.1-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.9.10"
