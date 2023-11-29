edac6f5cdb2f29aa3532f5afcd001c25fa5c78b1
Raphael Tarita <raphael.tarita@gmail.com>
2023-11-29 17:41:48 +0100

    version upgrades: kotlin: 1.9.10 -> 1.9.21 kotlinx-serialization: 1.9.10 -> 1.9.21 detekt: 1.23.1 -> 1.23.4 ktor: 2.3.4 -> 2.3.6 logback: 1.4.11 -> 1.4.13 skullgame-common: 1.0.1-SNAPSHOT -> 1.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.9.10"
-val ktorVersion = "2.3.4"
-val logbackVersion = "1.4.11"
+val kotlinVersion = "1.9.21"
+val ktorVersion = "2.3.6"
+val logbackVersion = "1.4.13"
 val kotlinxDatetimeVersion = "0.4.1"
-val skullgameCommonVersion = "1.0.1-SNAPSHOT"
+val skullgameCommonVersion = "1.0.2-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.4"
-    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
+    id("io.ktor.plugin") version "2.3.6"
+    id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
 
 group = "com.rtarita"
