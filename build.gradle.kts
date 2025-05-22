4cbd9e7d5c025d317db1723ca866fd2ee95f53d2
Raphael Tarita <raphael.tarita@gmail.com>
2025-05-22 08:40:41 +0200

    fix: set java target versions to make build independent from gradle JDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,11 @@ tasks.withType<KotlinCompile>().all {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 detekt {
     basePath = rootDir.toString()
 }
